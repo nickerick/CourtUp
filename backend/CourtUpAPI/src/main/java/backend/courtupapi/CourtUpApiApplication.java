8b9514aed04a72f87c7f7949c28d85e5afd9d223
nickerick <nickerickson737@gmail.com>
2022-10-24 00:10:18 -0500

    initializes project files

---
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/CourtUpApiApplication.java
@@ -0,0 +1,13 @@
+package backend.courtupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtUpApiApplication.class, args);
+	}
+
+}
---
+++ backend/CourtUpAPI/src/test/java/backend/courtupapi/CourtUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.courtupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8d3c3ceca44a78ad5f25fcb4ca51f790c760ab4c
nickerick <nickerickson737@gmail.com>
2022-10-24 21:35:43 -0500

    adds initial test POST request - first rouondttrip woo

---
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/court/CourtController.java
@@ -0,0 +1,22 @@
+package backend.courtupapi.court;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourtController {
+
+    @Autowired
+    CourtRepository courtRepository;
+
+    @PostMapping("/court")
+    String createCourt(@RequestParam("name") String name) {
+        Court myCourt = new Court();
+        myCourt.setName(name);
+        courtRepository.save(myCourt);
+
+        return "good";
+    }
+}
---
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/court/CourtRepository.java
@@ -0,0 +1,8 @@
+package backend.courtupapi.court;
+
+import org.springframework.data.jpa.repository.*;
+
+public interface CourtRepository extends JpaRepository<Court, Long> {
+
+}
+

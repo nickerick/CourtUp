636a917a30ceea47ce0920f95ed5a504023abf53
nickerick <nickerickson737@gmail.com>
2022-11-12 12:16:37 -0600

    fixes issue

--- frontend/court-up/src/pages/Home/HomeBody.js
+++ frontend/court-up/src/pages/Home/HomeBody.js
@@ -1,8 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-<<<<<<< HEAD
 import './Home.css';
-
 
 function HomeBody(props) {
     return (
@@ -16,26 +14,7 @@ function HomeBody(props) {
                     <Button  className='btnPrimary btn mt-5 mb-2'> Courts Near Me</Button>
                     <Button  className= 'btnSecondary btn my-2 '>Search Courts</Button>
                 </Card.Body>
-                </Card>
-        
-=======
-
-function HomeBody(props) {
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="courtup-logo.png/100px180" />
-                <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-        </div>
->>>>>>> main
+                </Card> 
     )
 }
 

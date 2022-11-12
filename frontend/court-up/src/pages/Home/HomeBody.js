e94189d04c17a09cc350ab6ea43584dd64312815
nickerick <nickerickson737@gmail.com>
2022-11-11 23:09:33 -0600

    adding bootstraps

--- frontend/court-up/src/pages/Home/HomeBody.js
+++ frontend/court-up/src/pages/Home/HomeBody.js
@@ -1,6 +1,21 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 function HomeBody(props) {
     return (
-        <div> body</div>
+        <div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="courtup-logo.png/100px180" />
+                <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
--- frontend/court-up/src/pages/Home/HomePage.js
+++ frontend/court-up/src/pages/Home/HomePage.js
@@ -1,8 +1,9 @@
+import HomeBody from "./HomeBody";
 
 function HomePage(props) {
     return (
         <div>
-            Page
+            <HomeBody /> 
         </div>
     )
 }

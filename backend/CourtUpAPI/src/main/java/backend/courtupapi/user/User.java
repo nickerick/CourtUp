5f234131a4677622f9fc7f975c39d7040a166336
nickerick <nickerickson737@gmail.com>
2022-11-12 21:46:10 -0600

    creates user model

---
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/user/User.java
@@ -0,0 +1,65 @@
+package backend.courtupapi.user;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String passSalt;
+
+    public User() {}
+
+    public User(String email, String username, String password, String passSalt) {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassSalt() {
+        return passSalt;
+    }
+
+    public void setPassSalt(String passSalt) {
+        this.passSalt = passSalt;
+    }
+
+}

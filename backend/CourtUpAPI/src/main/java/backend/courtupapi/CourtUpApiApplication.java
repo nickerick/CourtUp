4b293166794d5669fc246328b3ce88f79f006908
nickerick <nickerickson737@gmail.com>
2023-01-21 16:04:30 -0600

    sets up proxy on frontend + adds /api context + removes unneeded cors

--- backend/CourtUpAPI/src/main/java/backend/courtupapi/CourtUpApiApplication.java
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/CourtUpApiApplication.java
@@ -13,13 +13,4 @@ public class CourtUpApiApplication {
 		SpringApplication.run(CourtUpApiApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
 }

602f5d7e35c5e1041ab719341a8ac99b83b28086
nickerick <nickerickson737@gmail.com>
2023-01-21 22:07:59 -0600

    LETS GO - adds final touches + settings to package react with springboot into 1 jar

--- backend/CourtUpAPI/src/main/java/backend/courtupapi/user/UserController.java
+++ backend/CourtUpAPI/src/main/java/backend/courtupapi/user/UserController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("api")
 public class UserController  {
 
     @Autowired

31630c2f14889d8df382860fd86f8bf0a4fc7af4
nickerick <nickerickson737@gmail.com>
2023-01-22 17:45:54 -0600

    adds frontend routing

--- frontend/court-up/src/index.js
+++ frontend/court-up/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import HomePage from './pages/Home/HomePage';
+import LoginPage from './pages/Login/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
+    </Router>  
   </React.StrictMode>
 );
 
+  
---
+++ frontend/court-up/src/pages/Login/LoginPage.js
@@ -0,0 +1,9 @@
+function LoginPage(props) {
+    return (
+        <div>
+            <h1>login page bruh</h1>
+        </div>
+    )
+}
+
+export default LoginPage;

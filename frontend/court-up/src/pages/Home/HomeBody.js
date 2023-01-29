c353acb9766859fecfa2cc15e11343c91f38d6fc
nickerick <nickerickson737@gmail.com>
2023-01-28 23:14:03 -0600

    fixing button

--- frontend/court-up/src/pages/Home/HomeBody.js
+++ frontend/court-up/src/pages/Home/HomeBody.js
@@ -18,7 +18,7 @@ function HomeBody(props) {
                     <Card.Text className='my-5'>
                     Find, rate, and play at basketball courts across the globe!
                     </Card.Text>
-                    <Button  className='btnPrimary btnHomeBody mt-5 mb-1' onClick={testDb}> Courts Near Me</Button>
+                    <Button  className='btnPrimary btnHomeBody mt-5 mb-1' onClick={exampleFunc}> Courts Near Me</Button>
                     <Button  className= 'btnSecondary btnHomeBody my-3 '>Search Courts</Button>
                 </Card.Body>
                 </Card>
